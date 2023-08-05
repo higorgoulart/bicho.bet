e437c9b1f1eb09dcea4012ac4b4d71f52bccbc33
higorgoulart <higorgoulart@hotmail.com>
2023-08-05 18:09:38 -0300

    add images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import gorila from './assets/gorila.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <h1 className="text-pink-700">BICHO.BET</h1>
+        <div className="alert alert-success fixed flex top-0 left-0 items-center justify-center border-none">
+            <span>Cadastre-se e receba um bônus de até R$ 200,00! 🔥</span>
+            <a href="" className="btn btn-sm btn-primary">Receber</a>
+        </div>
+        <img src={gorila} alt="Gorila" />
+        <h1 className="text-primary">BICHO.BET</h1>
         <span className="loading loading-infinity loading-lg"></span>
     </>
   )
