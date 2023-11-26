17abd2186661049b22cecf29277f698fd9f7d44c
Higor Goulart Massiroli <109246101+higorgoulart@users.noreply.github.com>
2023-11-26 14:46:23 -0300

    Update MenorQueTresVezesDepositadoException.java

--- springboot-api/src/main/java/com/bicho/bet/exceptions/MenorQueTresVezesDepositadoException.java
+++ springboot-api/src/main/java/com/bicho/bet/exceptions/MenorQueTresVezesDepositadoException.java
@@ -2,6 +2,6 @@ package com.bicho.bet.exceptions;
 
 public class MenorQueTresVezesDepositadoException extends RuntimeException {
     public MenorQueTresVezesDepositadoException() {
-        super("Saldo menor que 3 vezes o valor depositado. Ganhe para sacar!");
+        super("Valor menor que 3 vezes a quantidade depositada. Ganhe para sacar!");
     }
 }
