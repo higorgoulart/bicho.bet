83df3c3354c14d735952a420cf7ce3f1ccbbba8a
Guilherme <guilhermesavio02@hotmail.com>
2023-11-26 15:53:37 -0300

    mod run.py e spring database

--- run.py
+++ run.py
@@ -5,29 +5,30 @@ def main():
     print("Iniciando Postgres...")
     subprocess.call("docker-compose up -d postgres", shell=True)
 
-    print("Aguardando Postgres...")
+    print("Aguardando o Postgres finalizar as configurações e estar disponível para conexões...")
     time.sleep(20)
 
     print("Iniciando Spring API...")
     subprocess.call("docker-compose up -d spring-api", shell=True)
-    print("Aguardando Spring API")
+    print("Aguardando Spring API...")
     time.sleep(20)
 
     print("Iniciando Django Server...")
     subprocess.call("docker-compose up -d django-server", shell=True)
-    print("Aguardando Django-Server")
+    print("Aguardando Django-Server...")
     time.sleep(20)
 
     print("Conectando ao contêiner do Django")
+    print("Para realizar as migrações digite: python manage.py migrate")
+    print("Para iniciar o processo de criação de um super-usuário digite: export DJANGO_SUPERUSER_PASSWORD={sua senha sem as chaves}")
+    print("Para finalizar o processo de criação de um super-usuário digite: python manage.py createsuperuser --username {seu usuario} --email {seu email} --noinput")
+    print("Após digitar os comandos, pressione CTRL+D para sair.")
     subprocess.call("docker exec -it django-server /bin/bash", shell=True)
-    print("Para realizar as migrações copie e cole: python manage.py migrate")
-    print("Para iniciar o processo de criação de um super-usuário copie e cole: export DJANGO_SUPERUSER_PASSWORD={sua senha}")
-    print("Para finalizar o processo de criação de um super-usuário copie e cole: python manage.py createsuperuser --username {seu usuario} --email {seu email} --noinput")
-    print("Para sair pressione: CTRL + D")
+    print("Super-usuário criado com sucesso.")
 
     print("Iniciando Go Scheduler...")
     subprocess.call("docker-compose up -d go-scheduler", shell=True)
-    print("Aguardando Go-Scheduler")
+    print("Aguardando Go-Scheduler...")
     time.sleep(20)
 
     print("Iniciando Frontend...")
